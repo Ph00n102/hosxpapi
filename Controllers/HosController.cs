5bcc6626db42db1efff60eaaf87449ca39acc7cf
Ph00n102 <phoonhd10@gmail.com>
2024-08-09 14:11:07 +0700

    api

--- Controllers/HosController.cs
+++ Controllers/HosController.cs
@@ -14,28 +14,47 @@ namespace HosApi.Controllers;
         }
 
          [HttpGet]
-        public IActionResult getHNdb (string paraHN)
+        public IActionResult getHos (string paraHN)
         { 
             //  var query = Ovst  Patient
             // from a in db.Wards
-             var query =
-            from a in db.Patients
-            join b in db.Ovsts on a.Hn equals b.Hn  
-
-            where a.Hn == paraHN 
+            if (paraHN.Length == 7)
+                {
+                var queryHN =
+                from a in db.Patients
+                join b in db.Ovsts on a.Hn equals b.Hn  
+                where a.Hn == paraHN 
+                    select new
+                    {
+                        b.Hn,
+                        a.Cid,
+                        a.Pname,
+                        a.Fname,
+                        a.Lname
+                        // c.Name,
+                        // c.OldCode
+                    };
+                    return Json(queryHN.Take(50));
+                    
+                }
+                DateOnly dateNow = DateOnly.FromDateTime(DateTime.Now);
+                var queryQN = 
+                from a in db.Ovsts
+                join b in db.Patients on  a.Hn equals b.Hn
+                where a.Vstdate == dateNow && Convert.ToString( a.Oqueue ) == paraHN 
+                select new
+                {
+                    a.Hn, 
+                    b.Pname, 
+                    b.Fname, 
+                    b.Lname,
+                    a.Vstdate
+                };
+                return Json(queryQN.Take(50));
+                
             // group a by a.Hn  into newGroup
+           
             
-            select new
-            {
-                b.Hn,
-                a.Cid,
-                a.Pname,
-                a.Fname,
-                a.Lname
-                // c.Name,
-                // c.OldCode
-            };
-            return Json(query.Take(50));
         }
          [HttpGet]       
         public IActionResult getQNdb(string paraQN)
