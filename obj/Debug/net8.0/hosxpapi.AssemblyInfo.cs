844b989aa390c901631519ed2ed85fdc7f669e04
coachnrm <nuttapong_coach@hotmail.com>
2024-09-20 11:54:32 +0700

    add get user

--- obj/Debug/net8.0/hosxpapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/hosxpapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hosxpapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f11db75b53422ecad13960d1852ee7cd2dddcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24977f60ce2d2aea74988e18ce6d16c377da97f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("hosxpapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hosxpapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
