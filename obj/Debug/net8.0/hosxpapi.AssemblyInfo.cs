f69ed03f7e07b73e591c03f6ca8eb60de9f83d96
coachnrm <nuttapong_coach@hotmail.com>
2024-09-26 10:48:00 +0700

    first commit

--- obj/Debug/net8.0/hosxpapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/hosxpapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hosxpapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81250cc9451034abc3e5938aa95231dfbec9da5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6541fee8ef57635a7c07e7cd453923e762358c92")]
 [assembly: System.Reflection.AssemblyProductAttribute("hosxpapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hosxpapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
