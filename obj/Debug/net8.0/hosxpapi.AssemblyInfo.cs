f2b8e917f64e1feff46d9f951e179d2a8e4496dc
coachnrm <nuttapong_coach@hotmail.com>
2024-10-21 16:21:10 +0700

    add code

--- obj/Debug/net8.0/hosxpapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/hosxpapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hosxpapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12308dbe64d6813f46c5516616370697c1290e5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff41cc18a2e8949efee825793982a796390074b")]
 [assembly: System.Reflection.AssemblyProductAttribute("hosxpapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hosxpapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
